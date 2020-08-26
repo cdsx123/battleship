dbbc43fb039af421f11bb44b5cb0d9b708e4290f
Ekam Sidhu <ekamsinghsidhu@yahoo.com>
2020-08-25 22:08:40 -0700

    Updates

---
+++ precache-manifest.dedc28ec2a7ecf27837fb61eea2f03c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6666349adaea8faf842864887ffd5ff7",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "c3463497613ce9a1d7ae",
+    "url": "/battleship/static/css/main.416a254b.chunk.css"
+  },
+  {
+    "revision": "83d4761b9a32abd438b7",
+    "url": "/battleship/static/js/2.c16121c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.c16121c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3463497613ce9a1d7ae",
+    "url": "/battleship/static/js/main.1102eff1.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
